08778804625b72aa598bc4eccc5ab0c13de9061d
Non-dual-being <kevindeschepper@hotmail.com>
2026-07-16 16:31:30 +0200

    fix: include flatpickr core styles in admin css

--- resources/js/admin.ts
+++ resources/js/admin.ts
@@ -1,4 +1,3 @@
-import "flatpickr/dist/flatpickr.min.css";
 import "../css/admin.css";
 import { initializeServerFlashAutoDismiss } from "./admin/flash/initializeServerFlashAutoDismiss";
 import { mountAdminDashboard } from "./admin/mountAdminDashboard";
